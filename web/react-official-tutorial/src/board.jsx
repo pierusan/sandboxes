ca1a36ef5049eb2a27b103aa5b7ce8909dde6b56
Pierre Amelot <pierre.p.amelot@gmail.com>
2020-09-18 18:53:03 +0800

    Split into jsx files to follow airbnb styleguide

---
+++ web/react-official-tutorial/src/board.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./index.css";
+
+/**
+ * Square is a 'function component' i.e. only has a render() method
+ * and does not have a state so can be written as a function
+ */
+function Square(props) {
+  const { value, onClick } = props;
+  return (
+    <button type="button" className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+}
+
+export default class Board extends React.Component {
+  renderSquare(i) {
+    const { squares, onClick } = this.props;
+    return <Square value={squares[i]} onClick={() => onClick(i)} />;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
