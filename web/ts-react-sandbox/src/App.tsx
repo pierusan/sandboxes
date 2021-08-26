ac337f9f665c89c9aea9fb145f880682ff43aeae
Pierre Amelot <pierre.amelot@ouster.io>
2021-08-26 13:50:00 -0700

    Format and remove link

--- web/ts-react-sandbox/src/App.tsx
+++ web/ts-react-sandbox/src/App.tsx
@@ -1,27 +1,27 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-const Parent : React.FC = ({children}) => {
-  const [parentCounter, setParentCounter] = useState(0)
+const Parent: React.FC = ({ children }) => {
+  const [parentCounter, setParentCounter] = useState(0);
 
   const randomNumber = Math.random();
 
-  return <div><p>{parentCounter}</p><p>{randomNumber}</p>{children}</div>
-}
+  return (
+    <div>
+      <p>{parentCounter}</p>
+      <p>{randomNumber}</p>
+      {children}
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Parent><p>Prout</p></Parent>
+        <Parent>
+          <p>Prout</p>
+        </Parent>
       </header>
     </div>
   );
