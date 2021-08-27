67f9a76f00a92bbcab888e4c30f733896b7470a5
Pierre Amelot <pierre.amelot@ouster.io>
2021-08-27 10:48:17 -0700

    Add console logs to illustrate react lifecycle

--- web/ts-react-sandbox/src/App.tsx
+++ web/ts-react-sandbox/src/App.tsx
@@ -1,28 +1,131 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import "./App.css";
 
-const Parent: React.FC = ({ children }) => {
-  const [parentCounter, setParentCounter] = useState(0);
+function sleep(milliseconds: number) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e8; i++) {
+    if (new Date().getTime() - start > milliseconds) {
+      break;
+    }
+  }
+}
 
+// parentCounter is a prop of ChildComponent
+const ChildComponent = ({ parentCounter }: { parentCounter: number }) => {
+  console.log("child render start");
+
+  const [childCounter, setChildCounter] = useState(0);
+  // Will change on every render
   const randomNumber = Math.random();
 
+  //////////////////////////////////////////////////////////////////////////////
+  // On first render
+  //////////////////////////////////////////////////////////////////////////////
+  useEffect(() => {
+    console.log("child useEffect[]");
+  }, []);
+
+  useLayoutEffect(() => {
+    console.log("child useLayoutEffect[]");
+  }, []);
+
+  //////////////////////////////////////////////////////////////////////////////
+  // On every render, when childCounter is updated
+  //////////////////////////////////////////////////////////////////////////////
+  useLayoutEffect(() => {
+    console.log("child useLayoutEffect[childCounter]");
+  }, [childCounter]);
+
+  useEffect(() => {
+    console.log("child useLayoutEffect[childCounter]");
+  }, [childCounter]);
+
+  console.log("child render end");
   return (
-    <div>
-      <p>{parentCounter}</p>
+    <div className="child-component">
+      <h2>And I'm the child 👶</h2>
+      <p>Parent Counter: {parentCounter}</p>
+      <p>Child Counter: {childCounter}</p>
+      <button
+        onClick={() => {
+          setChildCounter((prevCounter) => prevCounter + 1);
+        }}
+      >
+        Increment child counter
+      </button>
       <p>{randomNumber}</p>
-      {children}
+    </div>
+  );
+};
+
+let numRenders = 0;
+const ParentComponent: React.FC = ({ children }) => {
+  console.log("parent render start");
+
+  numRenders = numRenders + 1;
+
+  const [parentCounter, setParentCounter] = useState(0);
+  // Will change on every render
+  const randomNumber = Math.random();
+
+  //////////////////////////////////////////////////////////////////////////////
+  // On first render
+  //////////////////////////////////////////////////////////////////////////////
+  useEffect(() => {
+    console.log("parent useEffect[]");
+  }, []);
+
+  useLayoutEffect(() => {
+    console.log("parent useLayoutEffect[]");
+  }, []);
+
+  //////////////////////////////////////////////////////////////////////////////
+  // On every render, when parentCounter is updated
+  //////////////////////////////////////////////////////////////////////////////
+  useLayoutEffect(() => {
+    console.log("parent useLayoutEffect[parentCounter]");
+  }, [parentCounter]);
+
+  useEffect(() => {
+    console.log("parent useLayoutEffect[parentCounter]");
+  }, [parentCounter]);
+
+  console.log("parent render end");
+
+  return (
+    <div className="parent-component">
+      <h2>Sup! I am the parent 😎</h2>
+      <p>Parent Counter: {parentCounter}</p>
+      <button
+        onClick={() => {
+          console.log("Clicked parent counter. Will sleep");
+          setParentCounter((prevCounter) => {
+            console.log("Increment parent counter by 1");
+            return prevCounter + 1;
+          });
+          setParentCounter((prevCounter) => {
+            console.log("Increment parent counter by 1 again");
+            return prevCounter + 1;
+          });
+
+          // Turns out there is a queue for the state updater, so actually it
+          // will first sleep and then, except sometimes 🤷‍♂️
+          sleep(1000);
+        }}
+      >
+        Increment parent counter
+      </button>
+      <p>{randomNumber}</p>
+      <ChildComponent parentCounter={parentCounter} />
+      <p>Number of renders: {numRenders}</p>
     </div>
   );
 };
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Parent>
-          <p>Prout</p>
-        </Parent>
-      </header>
+    <div className="container">
+      <ParentComponent />
     </div>
   );
 }
